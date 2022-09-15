3976fac8f5983a94df8c3fd9dae084be17fdf80e
14261 <t#@vtsd.com>
2022-09-15 08:11:47 -0400

    Lab Done

--- Lab02vst.java
+++ Lab02vst.java
@@ -9,17 +9,21 @@
   public class Lab02vst {
 
     public static void main(String args[]) {
-      System.out.println("Lab 02,80 point version");
+      System.out.println("Lab 02,100 point version");
       System.out.println();
       System.out.println();
-      System.out.print("Measure today's knowledge");
+      System.out.print("Measure today's knowledge ");
       System.out.println("by yesterday's confusion");
       System.out.println();
       System.out.println("Bewilderment + Exposure = Obvious");
       System.out.println();
-      System.out.println("# #### ####    ##### ##### # #   # ##### #####");
-      System.out.println("# #       #    #   #     # # ##  #   #   #");
+      System.out.println("# ##### #####    ##### ##### # #   # ##### #####");
+      System.out.println("# #   # #   #    #   # #   # # ##  #   #   #");
+      System.out.println("# #   # #   #    ##### #   # # # # #   #   #####");
+      System.out.println("# #   # #   #    #     #   # # #  ##   #       #");
+      System.out.println("# ##### #####    #     ##### # #   #   #   #####"
 
+      );
     }
 
   }
